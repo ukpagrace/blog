92e650d00611835bb5ac3f68f3d7951c65041fc5
Ukpa Grace <ukpagraceu@gmail.com>
2024-10-19 09:05:39 +0100

    modified template

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,8 +1,15 @@
 // List of categories for blog posts
 export const CATEGORIES = [
-	'Category 1',
-	'Category 2',
-	'Category 3',
-	'Category 4',
-	'Category 5'
+	'Software Architecture',
+	'Personal Projects',
+	'Security',
+	'Cybersecurity',
+	'Data Structure and Algorithms',
+	'Databases and Data Storage',
+	'Cloud Computing',
+	'Programming Languages',
+	'System Design',
+	'Distributed Systems',
+	'Career Development',
+	'Open Source'
 ] as const
